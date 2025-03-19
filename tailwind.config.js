9f03492ae8281b2649a90e29b27e33159ec8fe7f
Tijo <hamsen3880@outlook.com>
2025-03-20 00:15:30 +0530

    landing page improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        glow: 'glow 1.5s infinite alternate',
+      },
+      keyframes: {
+        glow: {
+          '0%': { opacity: '0.4', transform: 'scale(1)' },
+          '100%': { opacity: '1', transform: 'scale(1.2)' },
+        }
+      }
+    }
   },
   plugins: [],
 }
