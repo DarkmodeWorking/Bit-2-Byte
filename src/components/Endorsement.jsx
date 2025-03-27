23f4ff0476dd5f232a624f6fd54cc78b7f4b5ee1
Tijo <hamsen3880@outlook.com>
2025-03-27 20:36:43 +0530

    endorsement fix

--- src/components/Endorsement.jsx
+++ src/components/Endorsement.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const teamMembers = [
-  {
-    id: 1,
-    name: 'Prof. Swarup Kr. Mitra',
-    department: 'Principal',
-    img: 'https://media.licdn.com/dms/image/v2/C5103AQFavRPZHUjWLA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1561011567550?e=1748476800&v=beta&t=p_S6_DFCQIMedTo4EFzmjA_78e-W2uMxDdL5PRoPkmI',
-    company: 'GNIT',
-    companyLogo: 'https://media.licdn.com/dms/image/v2/C4D0BAQGVdTueWoZ0Eg/company-logo_200_200/company-logo_200_200/0/1660843902959?e=1748476800&v=beta&t=_4TljX2Re_6WAhsqSmCyPZGe4FKItShpmyNX0woFc_I'
-  },
-  {
+	{
+		id: 1,
+		name: 'Prof. Swarup Kumar Mitra',
+		department: 'Principal',
+		img: 'https://media.licdn.com/dms/image/v2/C5103AQFavRPZHUjWLA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1561011567550?e=1748476800&v=beta&t=p_S6_DFCQIMedTo4EFzmjA_78e-W2uMxDdL5PRoPkmI',
+		company: 'GNIT',
+		companyLogo: 'https://media.licdn.com/dms/image/v2/C4D0BAQGVdTueWoZ0Eg/company-logo_200_200/company-logo_200_200/0/1660843902959?e=1748476800&v=beta&t=_4TljX2Re_6WAhsqSmCyPZGe4FKItShpmyNX0woFc_I'
+	},
+	{
 		id: 2,
 		name: 'Mr. Binit Kumar Mondal',
 		department: 'Co Organiser',
@@ -30,7 +30,7 @@ const Endorsement = () => {
 			</div>
 			<div className='grid grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6'>
 				{teamMembers.map((member, index) => {
-					let borderColor = 'border-gray-300'
+					let borderColor = 'border-white'
 					return (
 						<div key={member.id} className='flex flex-col items-center p-4 bg-zinc-900 rounded-xl shadow-md'>
 							<img
