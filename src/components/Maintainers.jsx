b1348095259721d7adbfef5ef8ba2099b5378d40
Tijo <hamsen3880@outlook.com>
2025-03-30 16:31:09 +0530

    modified maintainers view

--- src/components/Maintainers.jsx
+++ src/components/Maintainers.jsx
@@ -6,15 +6,15 @@ const teamMembers = [
 		name: 'Anurag Bhattacharjee',
 		department: 'aka GypsyDanger',
 		img: 'https://avatars.githubusercontent.com/u/185953875?v=4',
-		url:'https://github.com/DarkmodeWorking'
+		url: 'https://github.com/DarkmodeWorking'
 	},
 	{
 		id: 2,
 		name: 'Kaushik Kundu',
 		department: 'aka StrawHatPirate',
 		img: 'https://avatars.githubusercontent.com/u/105000989?v=4',
-		url:'https://github.com/kaushikkundu'
-	}
+		url: 'https://github.com/kaushikkundu'
+	},
 ]
 
 const Maintainers = () => {
@@ -31,20 +31,30 @@ const Maintainers = () => {
 					</button>
 				</a>
 			</div>
-			<div className='flex justify-center  items-center md:grid-cols-4 gap-4 sm:gap-6'>
-				{teamMembers.map((member, index) => {
-					return (
-						<div key={member.id} className='flex flex-col items-center p-4 bg-zinc-900 rounded-xl shadow-md cursor-pointer hover:border-4 border-cyan-400 border-2' onClick={()=> window.open(member.url,' _blank')}>
-							<img
-								src={member.img}
-								alt={member.name}
-								className='w-24 h-24 rounded-full object-cover border-4 border-violet-500'
-							/>
-							<h2 className='mt-3 text-[11px] sm:text-lg font-semibold'>{member.name}</h2>
-							<p className='text-[10px] sm:text-sm font-medium text-gray-400'>{member.department}</p>
-						</div>
-					)
-				})}
+			<div className='flex justify-center'>
+
+				<div className='grid md:grid-cols-2 gap-4 sm:gap-6'>
+					{teamMembers.map((member, index) => {
+						return (
+							<div
+								key={member.id}
+								className="relative group flex flex-col items-center p-4 rounded-xl shadow-md cursor-pointer border-transparent border-2 transition-all duration-300 hover:scale-105"
+								onClick={() => window.open(member.url, '_blank')}
+							>
+								<div className="absolute inset-0 -z-10 rounded-xl bg-cyan-400/50 blur-3xl scale-110 opacity-0 transition-all duration-300 group-hover:opacity-100"></div>
+								<div className="w-full relative flex flex-col items-center p-4 rounded-xl bg-zinc-900 cursor-pointer border-cyan-400 border-2">
+									<img
+										src={member.img}
+										alt={member.name}
+										className="w-24 h-24 rounded-full object-cover border-4 border-violet-400"
+									/>
+									<h2 className="mt-3 text-[11px] sm:text-lg font-semibold">{member.name}</h2>
+									<p className="text-[10px] sm:text-sm font-medium text-gray-400">{member.department}</p>
+								</div>
+							</div>
+						)
+					})}
+				</div>
 			</div>
 		</div>
 	)
